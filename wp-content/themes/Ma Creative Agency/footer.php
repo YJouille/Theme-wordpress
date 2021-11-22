4c4ea042581067e4a15bcf933c922fec16ce020b
LucasMR <lucas.mroy@gmail.com>
2021-11-22 11:44:06 +0100

    premerge

--- wp-content/themes/Ma Creative Agency/footer.php
+++ wp-content/themes/Ma Creative Agency/footer.php
@@ -1,19 +1,62 @@
+<div class="card mb-3" style="max-width: 540px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src=" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+</div>
 
-<?php
-if ( has_nav_menu( 'footer-menu' ) ) : ?>
-<?php
-wp_nav_menu ( array (
-'theme_location' => 'footer-menu' ,
-'container'      => 'nav',
-'menu_class'     => 'right no-bullets no-margin',
-) ); ?>
-<?php endif;
-?>
 
 
 
 
-    
+
+
+
+
+     <!-- ajout des widget  -->
+     <section class="top-footer">
+         
+ <?php if ( is_active_sidebar( 'footer_1' ) ) : ?>
+ <div id="footer_1" class="footer-widget widget-area" role="complementary">
+ <?php dynamic_sidebar( 'footer_1' ); ?>
+ </div>
+ <?php endif; ?>
+
+ <?php if ( is_active_sidebar( 'footer_2' ) ) : ?>
+ <div id="footer_2" class="footer-widget widget-area" role="complementary">
+ <?php dynamic_sidebar( 'footer_2' ); ?>
+ </div>
+ <?php endif; ?>
+     </section>
+
+
+     <section class="bottom-footer">
+
+ <?php if ( is_active_sidebar( 'footer_3' ) ) : ?>
+ <div id="footer_3" class="footer-widget widget-area" role="complementary">
+ <?php dynamic_sidebar( 'footer_3' ); ?>
+ </div>
+ <?php endif; ?>
+
+ <?php if ( is_active_sidebar( 'footer_4' ) ) : ?>
+ <div id="footer_4" class="footer-widget widget-area" role="complementary">
+ <?php dynamic_sidebar( 'footer_4' ); ?>
+ </div>
+ <?php endif; ?>
+ <?php //if (!function_exists('dynamic_sidebar') || !dynamic_sidebar('<b>footer_top'))?></b>
+ <!-- fin nouvelle widget area -->
   
 
+    
+</section>
 </body></html>
+
+
---
+++ wp-content/themes/Ma Creative Agency/script.js
@@ -0,0 +1,8 @@
+
+
+
+// document.querySelector('.scroll-down-button').addEventListener("click", function(){
+//     window.scrollTo(0, 600);
+// });
+
+// onClick="window.scrollTo(0, 600);"
