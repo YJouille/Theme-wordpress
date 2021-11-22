cbd12d39c1d24441a992f36146cc141db4130e1c
YJouille <y.j.39110@gmail.com>
2021-11-22 14:12:20 +0100

    commit after merge

--- wp-content/themes/Ma Creative Agency/single.php
+++ wp-content/themes/Ma Creative Agency/single.php
@@ -1,14 +1,5 @@
 <?php get_header(); ?>
 
-<div class="main single">
-
-    <?php if (have_posts()) : ?>
-        <?php while (have_posts()) ?>
-</div>
-^^^^^^^^ ici c'est avec la fonction get_header ^^^^^^
-
-
-
 
 <div class="container">
     <h1><?php the_title() ?></h1>
