cde3131a377a93c57f02250c4ee15af3bca23d06
LucasMR <lucas.mroy@gmail.com>
2021-11-22 14:04:16 +0100

    version revisée

--- wp-content/themes/Ma Creative Agency/footer.php
+++ wp-content/themes/Ma Creative Agency/footer.php
@@ -13,27 +13,6 @@
   </div>
 </div>
 
-     <!-- ajout des widget  -->
-     <section class="top-footer">
- <?php if ( is_active_sidebar( 'footer_1' ) ) : ?>
- <div id="footer_1" class="footer-widget widget-area" role="complementary">
- <?php dynamic_sidebar( 'footer_1' ); ?>
- </div>
- <?php endif; ?>
-
- <?php if ( is_active_sidebar( 'footer_2' ) ) : ?>
- <div id="footer_2" class="footer-widget widget-area" role="complementary">
- <?php dynamic_sidebar( 'footer_2' ); ?>
- </div>
- <?php endif; ?>
-     </section>
-
-
-     <section class="bottom-footer">
-
-
-
-
 
      <!-- ajout des widget  -->
      <section class="top-footer">
@@ -52,7 +31,7 @@
      </section>
 
 
-     <section class="bottom-footer">
+<section class="bottom-footer">
 
  <?php if ( is_active_sidebar( 'footer_3' ) ) : ?>
  <div id="footer_3" class="footer-widget widget-area" role="complementary">
@@ -65,10 +44,7 @@
  <?php dynamic_sidebar( 'footer_4' ); ?>
  </div>
  <?php endif; ?>
- <?php //if (!function_exists('dynamic_sidebar') || !dynamic_sidebar('<b>footer_top'))?></b>
  <!-- fin nouvelle widget area -->
-  
-
     
 </section>
 </body></html>
