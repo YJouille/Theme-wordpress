5327f8ee1c4e7fc62045bc826adf186fae32aa1e
JoselitoBTW <j.payet@codeur.online>
2021-11-24 13:39:11 +0100

    fin page contact

--- wp-content/themes/Ma Creative Agency/template_contact.php
+++ wp-content/themes/Ma Creative Agency/template_contact.php
@@ -5,6 +5,10 @@ include('header.php');
 
 <h1 id="titre"><?php the_title(); ?></h1>
 <?php the_content(); ?>
+<div class="contact">
+<?php echo do_shortcode('[contact-form-7 id="42" title="Formulaire de contact 1" html_class="form contact-form"]')?>
+</div>
+
 <div class="map">
 <h1 id="text_map">Retrouvez-nous</h1>
 <?php echo do_shortcode('[ultimate_maps id="1"]')?>
