29212546cc82e935cef17fd492e0a61a9640f45d
LucasMR <lucas.mroy@gmail.com>
2021-11-24 15:09:24 +0100

    integration contact

--- wp-content/themes/Ma Creative Agency/template_contact.php
+++ wp-content/themes/Ma Creative Agency/template_contact.php
@@ -1,14 +1,21 @@
 <?php
-/* Template name: Gabarit Contact */
+/* Template name: Gabarit Contact */ 
+include('header.php');
+?>
 
-get_header() ?>
+<h1 id="titre"><?php the_title(); ?></h1>
+<?php the_content(); ?>
+<div class="contact">
+<?php echo do_shortcode('[contact-form-7 id="42" title="Formulaire de contact 1" html_class="form contact-form"]')?>
+</div>
 
-    <h1 id="titre"><?php the_title() ?></h1>
-    <p><?php the_content() ?></p>
+<div class="map">
+<h1 id="text_map">Retrouvez-nous</h1>
+<?php echo do_shortcode('[ultimate_maps id="1"]')?>
+</div>
 
-<?php if ( is_active_sidebar( 'map' ) ) : ?>
-         <div id="map" class="footer-widget widget-area" role="complementary">
-         <?php dynamic_sidebar( 'map' ); ?>
-         </div>
-         <?php endif; ?>
-<?php get_footer() ?>
+
+
+<?php
+include('footer.php');
+?>
