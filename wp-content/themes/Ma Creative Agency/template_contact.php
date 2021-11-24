e0a2bb8d3f5d8ed6b53e5ff9fb4b7dd325e34622
JoselitoBTW <j.payet@codeur.online>
2021-11-24 11:56:30 +0100

    affichage carte

--- wp-content/themes/Ma Creative Agency/template_contact.php
+++ wp-content/themes/Ma Creative Agency/template_contact.php
@@ -5,6 +5,11 @@ include('header.php');
 
 <h1 id="titre"><?php the_title(); ?></h1>
 <?php the_content(); ?>
+<div class="map">
+<h1 id="text_map">Retrouvez-nous</h1>
+<?php echo do_shortcode('[ultimate_maps id="1"]')?>
+</div>
+
 
 
 <?php
