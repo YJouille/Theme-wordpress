49c53558b685ada5601c72c52cf1ea172c3bd2a4
LucasMR <lucas.mroy@gmail.com>
2021-11-24 13:51:46 +0100

    suppression doublon

--- wp-content/themes/Ma Creative Agency/footer.php
+++ wp-content/themes/Ma Creative Agency/footer.php
@@ -1,6 +1,5 @@
   <!-- ajout des widget  -->
   <section class="top-footer">
-<<<<<<< HEAD
     <div class="footer_left row">
       
       <?php if (is_active_sidebar('footer_1_img')) : ?>
@@ -49,39 +48,3 @@
   </body>
 
   </html>
-=======
-         
-         <?php if ( is_active_sidebar( 'footer_1' ) ) : ?>
-         <div id="footer_1" class="footer-widget widget-area" role="complementary">
-         <?php dynamic_sidebar( 'footer_1' ); ?>
-         </div>
-         <?php endif; ?>
-        
-         <?php if ( is_active_sidebar( 'footer_2' ) ) : ?>
-         <div id="footer_2" class="footer-widget widget-area" role="complementary">
-         <?php dynamic_sidebar( 'footer_2' ); ?>
-         </div>
-         <?php endif; ?>
-             </section>
-        
-        
-        <section class="bottom-footer">
-        
-         <?php if ( is_active_sidebar( 'footer_3' ) ) : ?>
-         <div id="footer_3" class="footer-widget widget-area" role="complementary">
-         <?php dynamic_sidebar( 'footer_3' ); ?>
-         </div>
-         <?php endif; ?>
-        
-         <?php if ( is_active_sidebar( 'footer_4' ) ) : ?>
-         <div id="footer_4" class="footer-widget widget-area" role="complementary">
-         <?php dynamic_sidebar( 'footer_4' ); ?>
-         </div>
-         <?php endif; ?>
-         <!-- fin nouvelle widget area -->
-            
-        </section>
-        <script src="<?php echo get_template_directory_uri(); ?>\script.js"></script>        
-        </body>        
-        </html>
->>>>>>> 25357288640ebe242ea47d57c481254d2742dfb4
